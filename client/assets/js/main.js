fe129013311fce838efc8ccfda57ec733bcb7f1f
Zylaroth <pavel2004162@gmail.com>
2023-11-14 21:06:24 +0300

    add styles

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -3,4 +3,20 @@ $(document).ready(function () {
         $(".navbar-active").toggleClass("is-active");
         $(".navbar-burger").toggleClass("is-active");
     });
+
+    var text = $("#typedText").text();
+    $("#typedText").text("");
+    var charCount = 0;
+    
+    function typeText() {
+        var slice = text.slice(0, charCount++);
+        $("#typedText").text(slice);
+    
+        if (charCount <= text.length) {
+            setTimeout(typeText, 50);
+        }
+    }
+    
+    typeText();
 });
+
