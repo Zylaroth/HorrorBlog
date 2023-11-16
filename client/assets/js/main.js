d04edbba7f3dc809528a92a2152256f188a9f925
Zylaroth <pavel2004162@gmail.com>
2023-11-16 17:00:29 +0300

    add carousel, table, modal

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,30 +1,61 @@
 $(document).ready(function () {
-    // Включение бургера
-    $(".navbar-burger").click(function () {
-        $(".navbar-active").toggleClass("is-active");
-        $(".navbar-burger").toggleClass("is-active");
-    });
+  // Включение бургера
+  $(".navbar-burger").click(function () {
+    $(".navbar-active").toggleClass("is-active");
+    $(".navbar-burger").toggleClass("is-active");
+  });
 
-    // Идентификатор слайдера
-    bulmaCarousel.attach('.carousel', {
-        slidesToScroll: 1,
-        slidesToShow: 1
-    });
+  $(".modal-active").click(function () {
+    $(".modal").toggleClass("is-active");
+  });
 
-    // Появление текста
-    var text = $("#typedText").text();
-    $("#typedText").text("");
-    var charCount = 0;
-    
-    function typeText() {
-        var slice = text.slice(0, charCount++);
-        $("#typedText").text(slice);
-    
-        if (charCount <= text.length) {
-            setTimeout(typeText, 50);
-        }
+  $(".modal-delete, .modal-cancel").click(function () {
+    $(".modal").removeClass("is-active");
+  });
+
+  let currentCarousel = 0;
+  const carousel = $('#carousel');
+  const carouselItems = $('.carousel');
+  const prevImage = $('#prev-image');
+  const nextImage = $('#next-image');
+
+  function updateCarousel() {
+    const translateValue = -currentCarousel * 100 + '%';
+    carousel.css('transform', 'translateX(' + translateValue + ')');
+  }
+
+  function updateSideImages() {
+    const prevImageSrc = (currentCarousel - 1 + carouselItems.length) % carouselItems.length;
+    const nextImageSrc = (currentCarousel + 1) % carouselItems.length;
+
+    prevImage.attr('src', carouselItems.eq(prevImageSrc).find('img').attr('src'));
+    nextImage.attr('src', carouselItems.eq(nextImageSrc).find('img').attr('src'));
+  }
+
+  function handleClick() {
+    $(this).attr('id') === 'prev' ? currentCarousel = (currentCarousel - 1 + carouselItems.length) % carouselItems.length : currentCarousel = (currentCarousel + 1) % carouselItems.length;
+    updateCarousel();
+    updateSideImages();
+  }
+
+  $('#prev, #next, #prev-image, #next-image').on('click', handleClick);
+
+  updateSideImages();
+
+  // Появление текста
+  var text = $("#typedText").text();
+  $("#typedText").text("");
+  var charCount = 0;
+
+  function typeText() {
+    var slice = text.slice(0, charCount++);
+    $("#typedText").text(slice);
+
+    if (charCount <= text.length) {
+      setTimeout(typeText, 50);
     }
-    
-    typeText();
+  }
+
+  typeText();
 });
 
